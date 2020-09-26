2883f0e35c6c9fb567834aa9acc19bc2dc450bb7
Anton Logvinenko <anton.logvinenko@gmail.com>
2020-09-27 01:42:19 +0300

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,12 @@
-mod conversion;
+mod custom_types_conversion;
 mod custom_types;
 mod primitives;
 mod printing;
 mod variable_binding;
+mod expressions;
 
 //get rid of println!s in primitives/printing - replace with assert_eq!
-//primitives: 2 + 5
-//custom types: 3 + 6
-//move variable binding after that
+//extract primitive types conversion
 
 //todo make code runnable?
 //todo better comments?
@@ -16,6 +15,7 @@ fn main() {
     printing::main();
     primitives::main();
     custom_types::main();
+    custom_types_conversion::main();
     variable_binding::main();
-    conversion::main();
+    expressions::main();
 }
