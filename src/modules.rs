18ca61b0a68e11000dd630cb8c623f59d8a78a62
Anton Logvinenko <anton.logvinenko@gmail.com>
2020-10-02 17:55:01 +0300

    Modules

--- src/modules.rs
+++ src/modules.rs
@@ -11,11 +11,9 @@
 
 //public structs with private fields can't be constructed using field names
 
-
 mod my_mod {
-     fn _private_function() {
 
-     }
+     fn _private_function() {}
 
      pub fn public_function() {
           //Private in public_nested
@@ -50,30 +48,17 @@ mod my_mod {
                pub_crate_function();
           }
 
-          fn _private_function() {
-               
-          }
+          fn _private_function() {}
 
-          pub fn public_function() {
+          pub fn public_function() {}
 
-          }
+          pub(self) fn _pub_self_function() {}
 
-          pub(self) fn _pub_self_function() {
-               
+          pub(super) fn pub_super_self_function() {}
 
-          }
-          
-          pub(super) fn pub_super_self_function() {
+          pub(in crate::modules::my_mod) fn pub_in_crate_my_mod_function() {}
 
-          }
-
-          pub(in crate::modules::my_mod) fn pub_in_crate_my_mod_function() {
-
-          }
-
-          pub(crate) fn pub_crate_function() {
-
-          }
+          pub(crate) fn pub_crate_function() {}
      }
 
      mod private_nested {
@@ -82,33 +67,54 @@ mod my_mod {
                _pub_self_function();
           }
 
-          fn _private_function() {
+          fn _private_function() {}
 
-          }
+          pub fn public_function() {}
 
-          pub fn public_function() {
-          }
+          pub(self) fn _pub_self_function() {}
 
-          pub(self) fn _pub_self_function() {
+          pub(super) fn pub_super_self_function() {}
 
-          }
-          
-          pub(super) fn pub_super_self_function() {
+          pub(in crate::modules::my_mod) fn pub_in_crate_my_mod_function() {}
 
-          }
-
-          pub(in crate::modules::my_mod) fn pub_in_crate_my_mod_function() {
-
-          }
-
-          pub(crate) fn pub_crate_function() {
-               
-          }
+          pub(crate) fn pub_crate_function() {}
      }
 }
 
+fn structures() {
+     mod my_mod {
+          pub struct OpenBox<T> {
+               pub contents: T,
+          }
+          pub struct ClosedBox<T> {
+               contents: T,
+          }
 
-pub fn main() {
+          impl<T> ClosedBox<T> {
+               pub fn new(contents: T) -> ClosedBox<T> {
+                    ClosedBox { contents: contents }
+               }
+
+               pub fn unclassify_contents(&self) -> &T {
+                    &self.contents
+               }
+          }
+     }
+
+     let open_box = my_mod::OpenBox {
+          contents: String::from("public"),
+     };
+     assert_eq!(open_box.contents, "public");
+     assert_eq!(open_box.contents, "public");
+     assert_eq!(open_box.contents, "public");
+
+     // let failed_closed_box = my_mod::ClosedBox {contents: "classified"};
+     let closed_box = my_mod::ClosedBox::new("classified");
+     // assert_eq!(closed_box.contents, "classified");
+     assert_eq!(closed_box.unclassify_contents(), &"classified");
+}
+
+fn mod_access() {
      /*
       * my_mod::
       */
@@ -134,13 +140,39 @@ pub fn main() {
      my_mod::public_nested::pub_crate_function();
 
      /*
-     * my_mod::private_nested
-     * Can't call anything inside private module
-     */
+      * my_mod::private_nested
+      * Can't call anything inside private module
+      */
      // my_mod::private_nested::private_function();
      // my_mod::private_nested::public_function();
      // my_mod::private_nested::pub_self_function();
      // my_mod::private_nested::pub_super_self_function();
      // my_mod::private_nested::pub_in_crate_my_mod_function();
      // my_mod::private_nested::pub_crate_function();
+}
+
+fn use_declaration() {
+     mod use_mod {
+          pub fn public_function() -> &'static str {
+               &"cake"
+          }
+     }
+
+     assert_eq!(use_mod::public_function(), "cake");
+     
+     {
+          use use_mod::public_function;
+          assert_eq!(public_function(), "cake")
+     }
+     
+     {
+          use use_mod::public_function as pf;
+          assert_eq!(pf(), "cake");
+     }
+}
+
+pub fn main() {
+     mod_access();
+     structures();
+     use_declaration();
 }
